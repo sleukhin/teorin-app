9ab9514d3757390e934d056197017598b096cbcb
SAVA\saveliy <zlinilla@gmail.com>
2018-03-29 09:07:30 +0300

    Move baseUrl to secret

--- src/shared/axios-teorin.js
+++ src/shared/axios-teorin.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import { BASE_URL } from '../secret.js';
 
 const instance = axios.create({
-  baseURL: 'https://teorin-core.firebaseio.com/'
+  baseURL: BASE_URL
 });
 
 export default instance;
